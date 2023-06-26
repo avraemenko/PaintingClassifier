c47cc270b9675fc447efada078a48b2ef87b6091
Kateryna Avramenko <kavramenko2000@gmail.com>
2023-06-27 01:04:45 +0300

    Initial Commit

---
+++ PaintingClassifier/PaintingClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintingClassifierApp.swift
+//  PaintingClassifier
+//
+//  Created by Kateryna Avramenko on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaintingClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
