c7ca301c52a39b9b86d305ef6271ea2bbc6f4c61
Kateryna Avramenko <kavramenko2000@gmail.com>
2023-06-27 18:22:57 +0300

    classification and ui added

--- PaintingClassifier/ContentView.swift
+++ PaintingClassifier/ContentView.swift
@@ -1,26 +1,93 @@
-//
-//  ContentView.swift
-//  PaintingClassifier
-//
-//  Created by Kateryna Avramenko on 27.06.2023.
-//
-
 import SwiftUI
+import UIKit
+import CoreML
+import Vision
+
+
 
 struct ContentView: View {
+    @State private var image: UIImage? = nil
+    @State private var showImagePicker: Bool = false
+    @State private var classificationLabel: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Text("Painter Classifier")
+                .font(.largeTitle)
+                .padding()
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Image(systemName: "paintpalette.fill")
+                .font(.system(size: 100))
+                .foregroundColor(.blue)
+            
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                    .clipped()
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 5)
+                    )
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.blue, lineWidth: 5)
+                    .frame(width: 300, height: 300)
+                    .overlay(
+                        Text("✨your painting✨")
+                            .foregroundColor(.blue)
+                    )
+            }
+
+            Button("Select painting") {
+                self.showImagePicker = true
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+
+            Text("Painted by: \(classificationLabel)")
+                .padding()
+        }
+        .sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+            ImagePicker(selectedImage: self.$image)
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage = image else { return }
+        classify(image: inputImage)
+    }
+
+    func classify(image: UIImage) {
+        // Load the ML model file
+        guard let model = try? VNCoreMLModel(for: ArtistClassifier().model) else {
+            self.classificationLabel = "Failed to load model"
+            return
+        }
+        
+        // Create a request for the ML model
+        let request = VNCoreMLRequest(model: model) {  request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                  let topResult = results.first else {
+                self.classificationLabel = "Classification failed"
+                return
+            }
+            DispatchQueue.main.async {
+                self.classificationLabel = topResult.identifier.replacingOccurrences(of: "_", with: " ")
+            }
+        }
+        
+        // Perform the request
+        guard let ciImage = CIImage(image: image) else { return }
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Failed to perform classification.\n\(error.localizedDescription)")
+        }
     }
 }
---
+++ PaintingClassifier/ImagePicker.swift
@@ -0,0 +1,50 @@
+//
+//  ImagePicker.swift
+//  iDresser
+//
+//  Created by Kateryna Avramenko on 27.01.23.
+//
+
+import SwiftUI
+import UIKit
+import CoreML
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var selectedImage: UIImage?
+
+        init(_ selectedImage: Binding<UIImage?>) {
+            _selectedImage = selectedImage
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            selectedImage = image
+            picker.dismiss(animated: true)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(_selectedImage)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+
+    init(selectedImage: Binding<UIImage?>) {
+        _selectedImage = selectedImage
+    }
+}
